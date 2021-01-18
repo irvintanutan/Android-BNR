9d40ba3148d6d95e64e9f99eca6ec70f4c72c02e
Irvin Tanutan <itanutan@placester.com>
2021-01-18 13:49:43 +0800

    Printer and Url

--- app/src/main/java/com/novigosolutions/certiscisco_pcsbr/zebra/Template.java
+++ app/src/main/java/com/novigosolutions/certiscisco_pcsbr/zebra/Template.java
@@ -51,6 +51,7 @@ public class Template {
                 "   </style>" +
                 "</head>\n" +
                 "    <body>\n" +
+                "        <br><br><br>\n" +
                 "        <img src=\"data:image/jpg;base64," + print.getLogo() + "\">\n" +
                 "\n" +
                 "<p>" + print.getCertisAddress() + "</p>\n" +
